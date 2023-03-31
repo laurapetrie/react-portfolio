3cc421787e99c58b217650d407a20864edef918b
laurapetrie <80486626+laurapetrie@users.noreply.github.com>
2023-03-31 15:01:29 +0100

    Set up home component and edited a few things

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,8 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  return <div className="Navbar">Navbar</div>;
+}
+
+export default Navbar;
