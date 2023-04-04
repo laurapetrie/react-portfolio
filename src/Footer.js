2a1f437628e7eababa1ad8054c88cb86e769fff2
laurapetrie <80486626+laurapetrie@users.noreply.github.com>
2023-04-04 19:24:29 +0100

    Added home content and some styling

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="Footer">
-      <h1>Footer</h1>
+      <p>Footer</p>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,57 @@
+import "./Home.css";
+import ProfileImage from "./profile-image.jpg";
+
 function Home() {
   return (
     <div className="Home">
-      <h1>Home</h1>
+      <div className="container">
+        <div className="row">
+          <div className="home-text col-8">
+            <h2>Hello! I'm Laura</h2>
+            <h1>A frontend developer based in Leeds, UK</h1>
+            <h3>
+              I am passionate about web development, building connections and
+              solving problems in a creative way
+            </h3>
+          </div>
+          <div className="col-4">
+            <img
+              src={ProfileImage}
+              className="profile-picture"
+              alt="Laura Petrie"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="home-socials">
+        <div class="home-social">
+          <a
+            href="https://github.com/laurapetrie"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i className="fa-brands fa-square-github"></i>
+          </a>
+        </div>
+        <div class="home-social">
+          <a
+            href="https://www.linkedin.com/in/laurapetrie/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+        </div>
+        <div class="home-social">
+          <a
+            href="https://www.instagram.com/rosy_codes/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i className="fa-brands fa-square-instagram"></i>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
