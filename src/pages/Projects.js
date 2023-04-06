a84b0aaaf38dda7458af8d19be6362f102932fab
laurapetrie <80486626+laurapetrie@users.noreply.github.com>
2023-04-06 21:27:00 +0100

    Started added images into projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,29 @@
 import "./Projects.css";
+import ReactWeatherApp from "./react-weather-app.png";
+import ReactDictionaryApp from "./react-dictionary-app.png";
 
 function Projects() {
   return (
     <div className="Projects">
       <div className="container">
         <h1>Projects</h1>
-        <div className="row">
-          <div className="col-6"></div>
-          <div className="col-6"></div>
+        <div className="container projects-container">
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src={ReactWeatherApp}
+                className="project-img img-fluid"
+                alt="react-weather-app"
+              />
+            </div>
+            <div className="col-md-6">
+              <img
+                src={ReactDictionaryApp}
+                className="project-img img-fluid"
+                alt="react-dictionary-app"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
