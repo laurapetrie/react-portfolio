cd355403df5aba88bc4b8d0e8cf5a0619bd7e8e4
laurapetrie <80486626+laurapetrie@users.noreply.github.com>
2023-04-06 20:47:54 +0100

    Added Projects and Contact pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,45 @@
+import "./Contact.css";
+
 function Contact() {
   return (
     <div className="Contact">
-      <h1>Contact</h1>
+      <div className="container">
+        <h1>Contact me</h1>
+        <h2 className="mb-5">I'd love to connect with you...</h2>
+        <div className="contact-socials">
+          <p>
+            <a href="mailto:laurajrpetrie@gmail.com">laurajrpetrie@gmail.com</a>
+          </p>
+          <p>
+            <a
+              href="https://github.com/laurapetrie"
+              rel="noreferrer"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </p>
+
+          <p>
+            <a
+              href="https://www.linkedin.com/in/laurapetrie/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://www.instagram.com/rosy_codes/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Instagram
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,15 @@
+import "./Projects.css";
+
 function Projects() {
   return (
     <div className="Projects">
-      <h1>Projects</h1>
+      <div className="container">
+        <h1>Projects</h1>
+        <div className="row">
+          <div className="col-6"></div>
+          <div className="col-6"></div>
+        </div>
+      </div>
     </div>
   );
 }
