254f8c0157dd7eff75db5fb90e633dc62f03948a
laurapetrie <80486626+laurapetrie@users.noreply.github.com>
2023-04-11 22:08:41 +0100

    Updated contact socials

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,22 @@
 import "./Contact.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
   return (
     <div className="Contact">
       <div className="main">
         <h1>Contact me</h1>
-        <h2 className="mb-5">I'd love to connect with you...</h2>
+        <h2 className="mb-4">I'd love to connect with you...</h2>
         <div className="contact-socials">
           <p>
-            <a href="mailto:laurajrpetrie@gmail.com">laurajrpetrie@gmail.com</a>
+            <a href="mailto:laurajrpetrie@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} /> laurajrpetrie@gmail.com
+            </a>
           </p>
           <p>
             <a
@@ -40,6 +44,11 @@ function Contact() {
               target="_blank"
             >
               <FontAwesomeIcon icon={faInstagramSquare} /> Instagram
+            </a>
+          </p>
+          <p>
+            <a href="/" rel="noreferrer" target="_blank">
+              <FontAwesomeIcon icon={faFile} /> Curriculum Vitae
             </a>
           </p>
         </div>
